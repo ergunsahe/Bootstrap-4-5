6f84834db3cf8c1881cad99f014d9286fcb623e8
ergunsahe <66415270+ergunsahe@users.noreply.github.com>
2021-01-03 23:59:18 +0100

    add features, footer section

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+  // Auto play modal video
+  $(".video").click(function () {
+    var theModal = $(this).data("target"),
+    videoSRC = $(this).attr("data-video"),
+    videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+    $(theModal + ' iframe').attr('src', videoSRCauto);
+    $(theModal + ' button.close').click(function () {
+      $(theModal + ' iframe').attr('src', videoSRC);
+    });
+  });
+});
+
+jQuery(".modal-backdrop, #videoModal .close, #videoModal .btn").live("click", function() {
+  jQuery("#videoModal iframe").attr("src", jQuery("#videoModal iframe").attr("src"));
+});
