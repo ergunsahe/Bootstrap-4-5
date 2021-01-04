97ab4936e72816e76f8df1ae958fe90ed5165add
ergunsahe <66415270+ergunsahe@users.noreply.github.com>
2021-01-04 01:53:21 +0100

    feat: add effect to scroll, responsive apperance and fix embed video closing problem

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function() {
   // Auto play modal video
   $(".video").click(function () {
-    var theModal = $(this).data("target"),
+    var theModal = $(this).data("bs-target"),
     videoSRC = $(this).attr("data-video"),
     videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
     $(theModal + ' iframe').attr('src', videoSRCauto);
@@ -11,6 +11,4 @@ $(function() {
   });
 });
 
-jQuery(".modal-backdrop, #videoModal .close, #videoModal .btn").live("click", function() {
-  jQuery("#videoModal iframe").attr("src", jQuery("#videoModal iframe").attr("src"));
-});
+
